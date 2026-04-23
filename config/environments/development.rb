365197ec69bd63014e5a7cfad67b382899907f83
Deepak Vishwakarma <vishwakarmadeepak310@gmail.com>
2026-04-23 19:25:44 +0530

    Update allowed hosts to use regex pattern

--- config/environments/development.rb
+++ config/environments/development.rb
@@ -44,5 +44,5 @@ Rails.application.configure do
   # Use an evented file watcher to asynchronously detect changes in source code,
   # routes, locales, etc. This feature depends on the listen gem.
   config.file_watcher = ActiveSupport::EventedFileUpdateChecker
-  config.hosts << "byteskilljudge001-production-07d4.up.railway.app"
+  config.hosts << /.*\.railway\.app/
 end
